35b5cf25190a44304ed5037e4478ee9cb7e4ee2a
tejuy123 <tejashwiniyacharya06@gmail.com>
2025-07-26 09:43:53 +0530

    Added I in Home for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const navItems = [
-    { name: "Home", href: "#home" },
+    { name: "Homeiiiii", href: "#home" },
     { name: "About", href: "#about" },
     { name: "Events", href: "#events" },
     { name: "Enrollment", href: "#selection" }, // Changed here
